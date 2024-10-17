13a4fde9387ee5554556de0bd46d77d0ba3aaf95
Dnann <m.ferdinanta@gmail.com>
2024-10-17 08:34:29 +0700

    Modified Tailwind Config

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Attribute;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/homepage', function () {
+    return view('livewire.homepage');
 });
