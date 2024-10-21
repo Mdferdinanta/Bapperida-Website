057526318cf68e7a38da936237623a539718148c
nmayzalia <aralia070503@gmail.com>
2024-10-21 15:15:56 +0700

    update pages

---
+++ resources/views/components/dokumen-card.blade.php
@@ -0,0 +1,11 @@
+<div class="space-y-4">
+    <div class="flex justify-between items-center bg-white p-4 shadow-md rounded-md mb-2">
+        <div>
+        <span class="text-blue-600 text-body ">Laporan Akhir Penyusunan Evaluasi Dokumen Kajian Risiko Bencana Dan Rencana Penanggulangan Bencana di Kota Bogor</span>
+        <p class="text-tiny mt-2">Tanggal Unggah</p>
+        </div>
+        <button class="bg-blue-500 text-white px-4 py-2 rounded-md flex items-center text-detail ">
+            Download <i class="ri-download-2-line ml-2"></i>
+        </button>
+    </div>
+</div>
