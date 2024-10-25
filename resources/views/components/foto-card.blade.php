74003d3b7879d87f326cde004af24e9dddc2bab5
nmayzalia <aralia070503@gmail.com>
2024-10-25 09:40:30 +0700

    all pages, kecuali profil

---
+++ resources/views/components/foto-card.blade.php
@@ -0,0 +1,9 @@
+<a href="{{ route('foto-preview') }}">
+    <div class="p-4 bg-white rounded-xs border border-mist-300 h-60">
+        <p class="text-detail mb-2">Tanggal</p>
+        <img src="{{ asset('assets/img/gallery-5.jpg') }}" class="w-full h-40 object-cover mb-2"
+            height="200" alt="video thumbnail">
+            <h3 class="text-body font-h3Bold mb-2">Judul Kegiatan</h3>
+            
+    </div>
+</a>
