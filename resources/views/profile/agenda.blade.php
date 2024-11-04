3637430c8c6d866781b9bcbaaa411a28754d510b
acrimsonmoon <khansanabila252@gmail.com>
2024-11-04 09:07:53 +0700

    page agenda

--- resources/views/profile/agenda.blade.php
+++ resources/views/profile/agenda.blade.php
@@ -9,31 +9,41 @@
         <main class="container mx-auto py-8 px-6">
 
             {{-- Search --}}
-            <div class="grid grid-cols-4 gap-16">
-                <div class="col-span-3">
-                    <div class="flex justify-end items-center mb-4">
-                        <div class="relative">
-                        </div>
+        <div class="grid grid-cols-4 gap-16">
+            <div class="col-span-3">
+                <div class="flex justify-end items-center mb-4">
+                    <div class="relative">
                     </div>
                 </div>
-                <div class="col-auto">
-                    <div class="flex justify-start items-center mb-4">
-                        <div class="relative">
-                            <h2 class="text-headline font-semibold">Berita Terbaru</h2>
-    
-            <div class=" grid grid-cols-4 gap-16">
-                <div class="col-span-3 grid grid-col-3 gap-8">
-                    {{-- calendar --}}
-                    <div class="bg-teal-200 col-span-1">
-                        <h2> bulan </h2>
+            </div>
+            <div class="col-auto">
+                <div class="flex justify-start items-center mb-4">
+                    <div class="relative">
+                        <h2 class="text-headline font-semibold">Berita Terbaru</h2>
                     </div>
+                </div>
+            </div>
+        </div>
 
-                    {{-- content --}}
-                    <div class="bg-gray-500 col-auto">
-                        <h1> divisi
-                        </h1>
-                    </div>
-                </div>
+        <div class="grid grid-cols-4 gap-8">
+            {{-- Section Tanggal --}}
+            <div class="col-span-1 rounded-lg bg-teal-100 p-4 h-[480px]">
+                <h2 class="text-lg font-semibold"></h2>
+            </div>
+
+            {{-- Jadwal Harian --}}
+            <div class="col-span-2">
+                <x-kegiatan-card></x-kegiatan-card>
+                <x-kegiatan-card></x-kegiatan-card>
+                <x-kegiatan-card></x-kegiatan-card>
+                <x-kegiatan-card></x-kegiatan-card>
+                <x-kegiatan-card></x-kegiatan-card>
+                <x-kegiatan-card></x-kegiatan-card>
+                <x-kegiatan-card></x-kegiatan-card>
+                <x-kegiatan-card></x-kegiatan-card>
+                <x-kegiatan-card></x-kegiatan-card>
+                <x-kegiatan-card></x-kegiatan-card>
+            </div>
               
                 {{-- Berita Section --}}
                 <div class="col-auto grid grid-rows-3 gap-4">
